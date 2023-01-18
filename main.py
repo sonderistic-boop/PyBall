5fb7fb83c221865f60b47da16825c0357812f945
sonderistic-boop <56941658+sonderistic-boop@users.noreply.github.com>
2023-01-18 10:53:41 +0000

    little fix

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import pygame
 
 pygame.init()
 
-running = False
 
 (width,height) = (1920,1080)
-newScreen = pygame.display.set_mode(width,height)
+newScreen = pygame.display.set_mode((width,height))
 
 def runtime():
     running = True
-    while running == True:
+    while running is True:
         pygame.display.flip()
 
 
