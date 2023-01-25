af4807597c7d8940912d29196c1f8b651d2ff1e5
sonderistic-boop <56941658+sonderistic-boop@users.noreply.github.com>
2023-01-25 10:57:22 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # PyBall
 Haxball-esque application implemented through python
-
-the dockerfile: https://dev.to/lowlighter/using-github-codespaces-with-vncflexbox-to-create-pokemon-maps-from-a-web-browser-3ch6
--- main.py
+++ main.py
@@ -1,18 +1,58 @@
-import pygame
+import pygame as pg
+import math
+import twisted
+import numpy as np
 
-pygame.init()
+
+from entities.pawn import Pawn
+pg.init() # initialises pg
 
 
-(width,height) = (1920,1080)
-newScreen = pygame.display.set_mode((width,height))
+colours = {
+    "red" : "#d14242",
+    "green" : "#52d142",
+    "blue" : "#52d142",
+    "yellow" : "#e1c16e",
+    "cyan" : "#03b9b9",
+    "magenta" : "#674ea7",
+    "orange" : "#e69138"
+
+}
+
+maxTicks = 60
+(WIDTH,HEIGHT) = (1200,600)
+screen = pg.display.set_mode((WIDTH,HEIGHT)) # width, height
+pg.display.set_caption("PyBall")
+
+
+field = pg.image.load("assets/fieldtile.jpg")
+
+clock = pg.time.Clock()
 
 def runtime():
+    
     running = True
-    while running is True:
-        pygame.display.flip()
+    newguy = Pawn(screen,"blue","bigboyo",True,(90,90))
+    while running:
+       
+        clock.tick(maxTicks)
+
+        for event in pg.event.get():
+            match event.type:
+                case pg.QUIT:
+                    running = False
+                #case...
+                    
+      
+        screen.fill((colours["green"]))
+        screen.blit(field,(20,20))
+        newguy.render()
+        pg.display.flip()
+       
 
 
-runtime()  
 
 
 
+if __name__ == "__main__":
+    runtime()
